6feec1b497e443ffdf02f32bbdf12b1fe77a918f
SheiinX <82066007+SheiinX@users.noreply.github.com>
2022-09-03 18:24:16 -0500

    Primera linea vertical

--- problem/program.asm
+++ problem/program.asm
@@ -1,4 +1,67 @@
-// COLOCA AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// COLOCA AQUÍ TU NOMBRE COMPLETO - Sebastián Franco Cataño
+// COLOCA AQUÍ TU ID - 000450587
+// COLOCA AQUÍ TU CORREO ELECTRÓNICO - sebastian.francoc@upb.edu.co
 
+(STR)
+        @KBD
+        D=M
+        @84
+        D=D-A
+        @DLINE
+        D;JEQ
+
+        @STR
+        0;JMP
+
+(DLINE)
+        @16400 //16384 (pantalla completa) + fila * 32 + columna/16
+        D=A
+	@coord 
+	M=D 
+	@256 //Las filas que se pintaran
+	D=A
+	@cont
+	M=D
+	@32
+	D=A 
+	@salto
+	M=D
+        @256
+        D=A
+	@color
+	M=D
+	@DRAW
+	0;JMP
+
+(DRAW)
+        @coord
+        D=M
+        @pscreen
+        M=D
+
+(DRAWL)
+        @cont
+        D=M
+        @END
+        D;JEQ
+        
+        @color
+        D=M
+        @pscreen
+        A=M
+        M=D
+
+        @cont
+        M=M-1
+
+        @salto
+        D=M
+
+        @pscreen
+        M=M+D
+
+        @DRAWL
+        0;JMP
+
+(END)
+        0;JMP
